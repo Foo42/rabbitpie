113d1efe50a9bfe94b3f5d63241b91e6de762572
Julian Haeger <julianhaeger@gmail.com>
2014-10-20 18:04:06 +0100

    moved topicEmitter to use newListener and removeListener events. worked out bugs (i think)

--- test/topicEmitter.js
+++ test/topicEmitter.js
@@ -82,19 +82,39 @@ describe('topic emitter', function () {
             emitter.once('foo.bar', function () {});
         });
 
-        it.skip('should unbind the underlying queue after the event fires when there are no other subscribers to that event', function (done) {
+        it('should unbind the underlying queue after the event fires when there are no other subscribers to that event', function (done) {
             var callbackWasCalled = false;
             var fakeQueue = new EventEmitter();
             fakeQueue.bind = function () {};
             fakeQueue.unbind = function (key) {
                 callbackWasCalled.should.equal(true);
                 key.should.equal('foo.bar');
+                EventEmitter.listenerCount(fakeQueue, 'message').should.equal(0);
                 done();
             };
 
             var emitter = topicEmitter.create(fakeQueue);
             emitter.once('foo.bar', function () {
                 callbackWasCalled = true;
+            });
+
+            emitter.emit('foo.bar');
+        });
+
+        it('should not unbind the underlying queue after the event fires when there are other subscribers to that event', function (done) {
+            var callbackWasCalled = false;
+            var fakeQueue = new EventEmitter();
+            fakeQueue.bind = function () {};
+            fakeQueue.unbind = function (key) {
+                done(new Error('should not have unbound'));
+            };
+
+            var emitter = topicEmitter.create(fakeQueue);
+            emitter.once('foo.bar', function () {
+                callbackWasCalled = true;
+            });
+            emitter.on('foo.bar', function () {
+                setTimeout(done, 100);
             });
 
             emitter.emit('foo.bar');
