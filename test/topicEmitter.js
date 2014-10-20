e7a4fbcc9b2b6d4309267fff6e33687d278a0317
Julian Haeger <julianhaeger@gmail.com>
2014-10-20 11:44:41 +0100

    modified topicEmitter to use newListener and removeListener events

--- test/topicEmitter.js
+++ test/topicEmitter.js
@@ -10,6 +10,7 @@ describe('topic emitter', function () {
                 key.should.equal('foo.bar');
                 done();
             };
+            fakeQueue.unbind = function () {};
 
             var emitter = topicEmitter.create(fakeQueue);
             emitter.on('foo.bar', function () {});
@@ -18,6 +19,7 @@ describe('topic emitter', function () {
         it('should emit subscribed event when message with that exact key arrives on queue', function (done) {
             var fakeQueue = new EventEmitter();
             fakeQueue.bind = function () {};
+            fakeQueue.unbind = function () {};
 
             var emitter = topicEmitter.create(fakeQueue);
             emitter.on('foo.bar', function () {
@@ -34,6 +36,7 @@ describe('topic emitter', function () {
                 it('should emit subscribed event when message which matches key arrives on queue', function (done) {
                     var fakeQueue = new EventEmitter();
                     fakeQueue.bind = function () {};
+                    fakeQueue.unbind = function () {};
 
                     var emitter = topicEmitter.create(fakeQueue);
                     emitter.on('a.b.*.d', function () {
@@ -50,6 +53,7 @@ describe('topic emitter', function () {
                 it('should emit subscribed event when message which matches key arrives on queue', function (done) {
                     var fakeQueue = new EventEmitter();
                     fakeQueue.bind = function () {};
+                    fakeQueue.unbind = function () {};
 
                     var emitter = topicEmitter.create(fakeQueue);
                     emitter.on('a.b.#.d', function () {
@@ -63,5 +67,37 @@ describe('topic emitter', function () {
             });
         });
 
+    });
+
+    describe('subscribing to events with once', function () {
+        it('should bind the underlying queue with the event namae', function (done) {
+            var fakeQueue = new EventEmitter();
+            fakeQueue.bind = function (key) {
+                key.should.equal('foo.bar');
+                done();
+            };
+            fakeQueue.unbind = function () {};
+
+            var emitter = topicEmitter.create(fakeQueue);
+            emitter.once('foo.bar', function () {});
+        });
+
+        it.skip('should unbind the underlying queue after the event fires when there are no other subscribers to that event', function (done) {
+            var callbackWasCalled = false;
+            var fakeQueue = new EventEmitter();
+            fakeQueue.bind = function () {};
+            fakeQueue.unbind = function (key) {
+                callbackWasCalled.should.equal(true);
+                key.should.equal('foo.bar');
+                done();
+            };
+
+            var emitter = topicEmitter.create(fakeQueue);
+            emitter.once('foo.bar', function () {
+                callbackWasCalled = true;
+            });
+
+            emitter.emit('foo.bar');
+        });
     });
 });
